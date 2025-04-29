713a5760b0b8a85f9565d0136d0dc54c6ecf155f
MietVdh <miet.vanderheyden@gmail.com>
2025-04-29 12:00:58 -0400

    Completed exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -20,12 +20,16 @@ const Feedback = (props) => {
 }
 
 const Statistics = ({ good, neutral, bad}) => {
+  const total = good + neutral + bad
   return (
     <div>
       <h2>Statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad)/total || 0}</p>
+      <p>positive {(good / total) * 100 || 0} %</p>
     </div>
   )
 }
