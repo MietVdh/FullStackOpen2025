b1cb0674caca145534a7fbeee711d6304cca1563
MietVdh <miet.vanderheyden@gmail.com>
2025-05-01 16:01:50 -0400

    Completed exercise 2.18

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Country from './components/Country'
+
+const App = () => {
+
+const [filter, setFilter] = useState('')
+const [countries, setCountries] = useState([])
+const [country, setCountry] = useState(null)
+
+const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
+
+const findAll = () => axios.get(`${baseUrl}/all`)
+const findCountry = name => axios.get(`${baseUrl}/name/${name}`)
+
+useEffect(() => {
+  if (filter) {
+    const containsFilter = (name) => {
+      name.toLowerCase().includes(filter.toLowerCase())
+    }
+
+    findAll().then(response => {
+      setCountries(response.data
+        .filter(o => o.name.common.toLowerCase().includes(filter.toLowerCase()))
+        .map(o => o.name.common)
+      )
+      countries.forEach((c) => {console.log(c)})
+    })
+  }
+}, [filter])
+
+useEffect(() => {
+  if (countries.length === 1) {
+    findCountry(countries[0])
+    .then((response) => {
+      console.log(response)
+      setCountry(response.data)
+      console.log(country)
+  })}
+}, [countries])
+
+const onFilterChange = (event) => {
+  console.log(event.target.value)
+  setFilter(event.target.value)
+}
+
+if (country) {
+  return (
+  <Country country={country} />
+)}
+
+return (
+  <div>
+    find countries: <input onChange={onFilterChange} value={filter}/>
+    {countries.length > 10 
+      ? <p>Too many matches, specify another filter</p>
+      : <ul>
+          {countries.map(c => <li key={c}>{c}</li>)}
+        </ul>
+    }
+  </div>
+)
+
+}
+
+
+
+export default App
---
+++ part2/countries/src/components/Country.jsx
@@ -0,0 +1,16 @@
+const Country = ({ country }) => {
+    return (
+        <div>
+            <h1>{country.name.common}</h1>
+            <p>Capital: {country.capital[0]}</p>
+            <p>Area: {country.area}</p>
+            <h2>Languages</h2>
+            <ul>
+                {Object.values(country.languages).map(l => <li key={l}>{l}</li>)}
+            </ul>
+            <img src={country.flags.png} alt={country.flags.alt} />
+        </div>
+    )
+}
+
+export default Country
