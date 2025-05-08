2c31c16afde49220a5c6b3e325e36f45edc083ca
MietVdh <miet.vanderheyden@gmail.com>
2025-05-08 11:30:37 -0400

    Completed exercise 4.4

---
+++ part4/bloglist/utils/list_helper.js
@@ -0,0 +1,13 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sumOfLikes, blog) => sumOfLikes + blog.likes, 0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
