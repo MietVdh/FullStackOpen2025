cb899dcd710bb4103943678e6818e8f79fbfc036
MietVdh <miet.vanderheyden@gmail.com>
2025-05-08 11:52:54 -0400

    Completed exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,12 +2,27 @@ const dummy = (blogs) => {
   return 1
 }
 
-
 const totalLikes = (blogs) => {
   return blogs.reduce((sumOfLikes, blog) => sumOfLikes + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+  tempFavorite = blogs[0]
+  tempLikes = blogs[0].likes
+  blogs.forEach((blog, idx) => {
+    if (blog.likes > tempLikes) {
+      tempFavorite = blog
+      tempLikes = blog.likes
+    }
+  })
+  return tempFavorite
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
