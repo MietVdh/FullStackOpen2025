8b7f4e7a84e65942e63a421f1143d3752b7ac68d
MietVdh <miet.vanderheyden@gmail.com>
2025-05-20 14:40:37 -0400

    Completed exercise 4.19

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -4,6 +4,8 @@ const errorHandler = (error, request, response, next) => {
       return response.status(400).json({ error: 'Username is not available' })
     } else if (error.name === 'ValidationError') {
       return response.status(400).json({ error: error.message })
+    } else if (error.name === 'JsonWebTokenError') {
+      return response.status(401).json({ error: 'token invalid' })
     }
   
   next(error)
